e275e6cceab9305a8d4fc0066f1dab8173b47507
Kim Gang Yeon <rkddus3177@naver.com>
2024-05-30 01:33:12 +0900

    ..

--- App.js
+++ App.js
@@ -1,21 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useCallback } from "react";
+import * as SplashScreen from "expo-splash-screen";
+import { Ionicons } from "@expo/vector-icons";
+import { useAssets } from "expo-asset";
+import { Text, View } from "react-native";
+import { useFonts } from "expo-font";
+import { NavigationContainer } from "@react-navigation/native";
+import Tabs from "./navigation/Tabs";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [fontsLoaded] = useFonts(Ionicons.font);
+  const [assets] = useAssets([require("./back.jpeg")]);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded && assets) await SplashScreen.hideAsync();
+  }, [fontsLoaded, assets]);
+
+  if (!fontsLoaded || !assets) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View onLayout={onLayoutRootView} style={{ flex: 1 }}>
+      <NavigationContainer>
+        <Tabs />
+      </NavigationContainer>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
